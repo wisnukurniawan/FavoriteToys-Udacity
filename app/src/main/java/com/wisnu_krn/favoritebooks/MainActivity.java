e821964d759db17054eb5ec3115adbbf2b566e44
wisnukurniawan <wisnuko13@gmail.com>
2016-12-01 21:13:32 +0700

    lesson 2: add menu

--- app/src/main/java/com/wisnu_krn/favoritebooks/MainActivity.java
+++ app/src/main/java/com/wisnu_krn/favoritebooks/MainActivity.java
@@ -1,25 +1,44 @@
 package com.wisnu_krn.favoritebooks;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import com.wisnu_krn.favoritebooks.models.ToyBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mToysListTextView;
+    private EditText mSearchBoxEditText;
+    private TextView mSearchResults;
+    private TextView mUrlDisplayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToysListTextView = (TextView) findViewById(R.id.tv_toy_name);
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        mSearchResults = (TextView) findViewById(R.id.tv_github_search_results_json);
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
 
-        String[] toyNames = ToyBox.getToyName();
+    }
 
-        for (String toyName : toyNames){
-            mToysListTextView.append(toyName + "\n\n\n");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if (menuItemThatWasSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
         }
+        return super.onOptionsItemSelected(item);
     }
 }
